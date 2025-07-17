4142197e525be56e63e409d027d36d98cab4572e
Lori Vel <tregoza@devmail.com>
2025-07-16 22:06:52 -0700

    add _redirects for route fix

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Tregoza Marketplace</h1>
+      <p>This is the real behind-the-scenes preview 🎯</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
